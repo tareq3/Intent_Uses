97c72a3e965584593d079c9a32f5321c047b2704
Tareq3 <mti.tareq3@gmail.com>
2018-07-04 23:35:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rakib/intent_uses/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.rakib.intent_uses;
+
+import android.content.Intent;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       
+    }
+}
