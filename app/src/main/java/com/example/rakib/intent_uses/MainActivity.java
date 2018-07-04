7ade31eed003af96d6e96740dc3e20fa1ff49600
Tareq3 <mti.tareq3@gmail.com>
2018-07-04 23:58:10 +0600

    2nd branch initial commit

--- app/src/main/java/com/example/rakib/intent_uses/MainActivity.java
+++ app/src/main/java/com/example/rakib/intent_uses/MainActivity.java
@@ -4,14 +4,30 @@ import android.content.Intent;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public  static  final String MY_KEY="my_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       
+       findViewById(R.id.my_Text).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+               Intent intent=new Intent(MainActivity.this, Main2Activity.class);
+
+               intent.putExtra(Intent.EXTRA_TEXT,"I am from 1"); //A inBuilt key. we can use that when we have to pass single element
+
+               intent.putExtra(MY_KEY,"My name is Tareq"); //custom Key Reciever Activity should have the Access of this key.
+
+               startActivity(intent);
+           }
+       });
     }
+
 }
